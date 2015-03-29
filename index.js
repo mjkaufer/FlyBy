cd42b12febe911febf5f59d6300734332ed566d4
mjkaufer <mjkaufer@gmail.com>
2015-03-29 13:39:20 -0400

    Work

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ client.takeOff();
 
 var currentImage = null;
 
-var frequency = 60;//pictures per hour
+var frequency = 120;//pictures per hour
 var save = false;
 
 var pngStream = client.getPngStream();
@@ -13,7 +13,16 @@ var pngStream = client.getPngStream();
 pngStream.on('data', function(img){
 	if(save == true){
 		//save the picture
+		var name = new Date().getTime();
+
+		require("fs").writeFile("images/" + name + ".png", img, 'base64', function(err) {
+			console.log(err);
+		});
+		console.log("Snap");
 		save = false;
+
+		//send twitter by reading from name.jpg
+
 	}
 
 });
@@ -21,4 +30,3 @@ pngStream.on('data', function(img){
 setInterval(function(){
 	save = true;
 }, 3600 * 1000 / frequency);
-
